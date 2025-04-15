634e18222e19b555fd272af433661299eb1e8563
Luc Lisi <llisi@mozilla.com>
2025-04-15 11:12:53 -0700

    PR comments

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,12 @@
 /* eslint @stylistic/quote-props: ["error", "consistent"] */
 
-import { addUseClientDirective } from './packages/build/plugins/addUseClientDirective'
+import { readFileSync, writeFileSync } from 'fs'
 
 import { defineConfig, Options } from 'tsup'
 
 const commonBundleConfig: Options = {
   target: 'esnext',
   platform: 'browser', // Ensure cross-platform modules import a browser-compatible package
-  splitting: true,
   clean: true,
   dts: false,
   minify: true,
@@ -22,13 +21,14 @@ const commonTypesConfig: Options = {
 const configs: Options[] = [
   {
     name: 'build',
+    splitting: true,
     entry: {
       'core': 'packages/core/src/index.ts',
       'react': 'packages/react/src/index.ts',
       'heyapi': 'packages/heyapi/src/index.ts',
     },
     format: ['esm', 'cjs'],
-    plugins: [addUseClientDirective(['dist/react'])],
+    plugins: [prependDirective('"use client"', ['dist/react'])],
     ...commonBundleConfig,
   },
   {
@@ -50,5 +50,27 @@ const configs: Options[] = [
     ...commonBundleConfig,
   },
 ]
+
+export function prependDirective(directive: string, clientLibs: string[]): NonNullable<Options['plugins']>[number] {
+  if (!Array.isArray(clientLibs)) {
+    throw Error('ClientLibs given to addUseClientDirective plugin must be an array.')
+  }
+  return {
+    name: 'add-use-client-directive',
+    // At the end of the build step, directly add the useClientDirective to files with specified pattern
+
+    buildEnd(ctx) {
+      for (const file of ctx.writtenFiles) {
+        for (const clientLibName of clientLibs) {
+          if (file.name.startsWith(clientLibName)) {
+            const fileContent = readFileSync(file.name, 'utf8')
+            writeFileSync(file.name, `${directive};${fileContent}`)
+            console.log(`Prepended "use client" directive to ${file.name}`)
+          }
+        }
+      }
+    },
+  }
+}
 
 export default defineConfig(configs)
