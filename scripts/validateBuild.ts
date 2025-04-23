0e88bd94931a8d5b1916032c575a4f2abf653f59
Luc Lisi <llisi@mozilla.com>
2025-04-23 15:44:42 -0700

    updating validateBuild

--- scripts/validateBuild.ts
+++ scripts/validateBuild.ts
@@ -32,16 +32,16 @@ function validateClientOnlyBuildFile(filePath: string) {
 }
 
 export function validateBuildFiles(expectedBuildOutput: ExpectedBuildOutput) {
-  const files = readdirSync(expectedBuildOutput.buildDir)
-  const filesSet = new Set()
-  for (const file of files) {
-    filesSet.add(file)
+  const filesInBuildDir = readdirSync(expectedBuildOutput.buildDir)
+  const filesInBuildDirSet = new Set()
+  for (const file of filesInBuildDir) {
+    filesInBuildDirSet.add(file)
     if (expectedBuildOutput.clientOnlyModules.includes(file)) {
       validateClientOnlyBuildFile(`${expectedBuildOutput.buildDir}${file}`)
     }
   }
   for (const requiredFile of expectedBuildOutput.files) {
-    if (!filesSet.has(requiredFile)) {
+    if (!filesInBuildDirSet.has(requiredFile)) {
       throw new MissingBuildFileError(Error(`Expected module ${requiredFile} not found in build directory ${expectedBuildOutput.buildDir}`))
     }
   }
