c7854426dec10cb90fdb21fd81e2349c5ab7a073
Luc Lisi <llisi@mozilla.com>
2025-04-23 11:21:00 -0700

    mocks not needed

--- scripts/test/mocks/mockFile.ts
+++
@@ -1,4 +0,0 @@
-export const mockFile = (name: string, content: string, type: string = 'text/plain') => {
-  const fileContent = new Blob([content], { type })
-  return new File([fileContent], name, { type })
-}
