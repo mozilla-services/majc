3ad0d8d09764b1a561b1752384fb13344bdf60a1
Masha Lifshin <mlifshin@mozilla.com>
2025-04-25 16:38:10 -0700

    Use getBoundingClientRect to avoid dealing with css string pixel values

--- examples/tests/helpers.ts
+++ examples/tests/helpers.ts
@@ -15,10 +15,9 @@ const expectAdLayout = async (page: Page, placementName: string, altText: string
 
   // Rounding the css widths and heights is needed here since some browsers (Mobile Safari) will
   // render the placement with a css dimension like '249.984375px' and that's close enough for us
-  const containerCSSWidth = await getStyle(container, 'width')
-  const containerCSSHeight = await getStyle(container, 'height')
-  expect(`${round(containerCSSWidth)}px`).toEqual(cssWidthForPlacement(placementName))
-  expect(`${round(containerCSSHeight)}px`).toEqual(cssHeightForPlacement(placementName))
+  const { width, height } = await getBoundingRectangle(container)
+  expect(Math.round(width)).toEqual(cssWidthForPlacement(placementName))
+  expect(Math.round(height)).toEqual(cssHeightForPlacement(placementName))
 }
 
 const expectClickNavigation = async (page: Page, altText: string): Promise<void> => {
@@ -35,41 +34,34 @@ const expectClickNavigation = async (page: Page, altText: string): Promise<void>
   expect(landingPage.url()).toEqual(normalizedHref)
 }
 
-const cssWidthForPlacement = (placementName: string): string => {
-  if (placementName.includes('billboard')) return `${FixedSize.Billboard.width}px`
-  if (placementName.includes('rectangle')) return `${FixedSize.MediumRectangle.width}px`
-  if (placementName.includes('skyscraper')) return `${FixedSize.Skyscraper.width}px`
-  if (placementName.includes('tile')) return '64px'
-  return 'Ceci n\'est pas un css width'
+const cssWidthForPlacement = (placementName: string): number => {
+  if (placementName.includes('billboard')) return FixedSize.Billboard.width
+  if (placementName.includes('rectangle')) return FixedSize.MediumRectangle.width
+  if (placementName.includes('skyscraper')) return FixedSize.Skyscraper.width
+  if (placementName.includes('tile')) return 64
+  return NaN
 }
 
-const cssHeightForPlacement = (placementName: string): string => {
-  if (placementName.includes('billboard')) return `${FixedSize.Billboard.height}px`
-  if (placementName.includes('rectangle')) return `${FixedSize.MediumRectangle.height}px`
-  if (placementName.includes('skyscraper')) return `${FixedSize.Skyscraper.height}px`
-  if (placementName.includes('tile')) return '64px'
-  return 'Ceci n\'est pas un css height'
-}
-
-const round = (cssDimension: string): number => {
-  if (!cssDimension.endsWith('px')) return -1
-  return Math.round(parseFloat(cssDimension))
+const cssHeightForPlacement = (placementName: string): number => {
+  if (placementName.includes('billboard')) return FixedSize.Billboard.height
+  if (placementName.includes('rectangle')) return FixedSize.MediumRectangle.height
+  if (placementName.includes('skyscraper')) return FixedSize.Skyscraper.height
+  if (placementName.includes('tile')) return 64
+  return NaN
 }
 
 /**
- * Retrieve a computed style for an element.
+ * Retrieve an element's bounding DOMRect
  *
- * @function getStyle
+ * @function getBoundingRectangle
  * @async
  * @param locator {Locator} The Playwright locator to evaluate (see: https://playwright.dev/docs/locators)
- * @param property {string} The CSS property for the style to retrieve
- * @returns Promise<string> The style value
+ * @returns Promise<DOMRect> The style value
  *
  * https://github.com/microsoft/playwright/issues/4282#issuecomment-718602677
  */
-export const getStyle = async (locator: Locator, property: string): Promise<string> => {
-  return locator.evaluate((el, property) => window.getComputedStyle(el)
-    .getPropertyValue(property), property)
+export const getBoundingRectangle = async (locator: Locator): Promise<DOMRect> => {
+  return locator.evaluate(el => el.getBoundingClientRect())
 }
 
 export { expectAdLayout, expectClickNavigation }
