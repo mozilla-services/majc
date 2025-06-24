b9dbc729a4d91affe835b037b424708f2302cea6
Masha Lifshin <mlifshin@mozilla.com>
2025-06-24 16:06:01 -0700

    Satisfy new linting rules

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,25 +1,23 @@
 import { ImageAd, Spoc, UaTile } from '@heyapi'
 
 // https://www.iab.com/wp-content/uploads/2019/04/IABNewAdPortfolio_LW_FixedSizeSpec.pdf
-export type IABAdUnitFormatType =
-  'Billboard' |
-  'SmartphoneBanner300' |
-  'SmartphoneBanner320' |
-  'Leaderboard' |
-  'SuperLeaderboardPushdown' |
-  'Portrait' |
-  'Skyscraper' |
-  'MediumRectangle' |
-  'TwentyBySixty' |
-  'MobilePhoneInterstitial640' |
-  'MobilePhoneInterstitial750' |
-  'MobilePhoneInterstitial1080' |
-  'FeaturePhoneSmallBanner' |
-  'FeaturePhoneMediumBanner' |
-  'FeaturePhoneLargeBanner'
+export type IABAdUnitFormatType = 'Billboard'
+  | 'SmartphoneBanner300'
+  | 'SmartphoneBanner320'
+  | 'Leaderboard'
+  | 'SuperLeaderboardPushdown'
+  | 'Portrait'
+  | 'Skyscraper'
+  | 'MediumRectangle'
+  | 'TwentyBySixty'
+  | 'MobilePhoneInterstitial640'
+  | 'MobilePhoneInterstitial750'
+  | 'MobilePhoneInterstitial1080'
+  | 'FeaturePhoneSmallBanner'
+  | 'FeaturePhoneMediumBanner'
+  | 'FeaturePhoneLargeBanner'
 
-export type NonIABAdUnitFormatType =
-  'NewTab'
+export type NonIABAdUnitFormatType = 'NewTab'
 
 export type AdUnitFormatType = IABAdUnitFormatType | NonIABAdUnitFormatType
 
@@ -33,12 +31,11 @@ export interface ImpressionThreshold {
   duration: number
 }
 
-export type IABContentTaxonomyType =
-  'IAB-1.0' |
-  'IAB-2.0' |
-  'IAB-2.1' |
-  'IAB-2.2' |
-  'IAB-3.0'
+export type IABContentTaxonomyType = 'IAB-1.0'
+  | 'IAB-2.0'
+  | 'IAB-2.1'
+  | 'IAB-2.2'
+  | 'IAB-3.0'
 
 export interface IABContent {
   taxonomy: IABContentTaxonomyType // https://iabtechlab.com/standards/content-taxonomy/
@@ -46,32 +43,30 @@ export interface IABContent {
 }
 
 // If you are adding a new log type, remember to include it in `docs/logging.md`
-export type LogType =
-  'logReporter.init.success' |
-  'recordClick.clickOccurred' |
-  'recordClick.success' |
-  'recordClick.callbackResponseError' |
-  'recordClick.invalidCallbackError' |
-  'renderPlacement.reportCallbackResponseError' |
-  'renderPlacement.reportCallbackInvalid' |
-  'renderPlacement.buildersReportCallbackError' |
-  'fetchAds.request.success' |
-  'fetchAds.request.error' |
-  'impressionObserver.recordImpression.viewed' |
-  'impressionObserver.recordImpression.callbackResponseError' |
-  'impressionObserver.recordImpression.invalidCallbackError' |
-  'impressionObserver.observeAd.adNotFoundError' |
-  'impressionObserver.forceRecordImpression.error' |
-  'placementComponent.adLoad.success' |
-  'placementComponent.adLoad.failure' |
-  'placementComponent.render.error'
+export type LogType = 'logReporter.init.success'
+  | 'recordClick.clickOccurred'
+  | 'recordClick.success'
+  | 'recordClick.callbackResponseError'
+  | 'recordClick.invalidCallbackError'
+  | 'renderPlacement.reportCallbackResponseError'
+  | 'renderPlacement.reportCallbackInvalid'
+  | 'renderPlacement.buildersReportCallbackError'
+  | 'fetchAds.request.success'
+  | 'fetchAds.request.error'
+  | 'impressionObserver.recordImpression.viewed'
+  | 'impressionObserver.recordImpression.callbackResponseError'
+  | 'impressionObserver.recordImpression.invalidCallbackError'
+  | 'impressionObserver.observeAd.adNotFoundError'
+  | 'impressionObserver.forceRecordImpression.error'
+  | 'placementComponent.adLoad.success'
+  | 'placementComponent.adLoad.failure'
+  | 'placementComponent.render.error'
 
-export type TelemetryEventLabel =
-  'init' |
-  'render_error' |
-  'ad_load_error' |
-  'fetch_error' |
-  'invalid_url_error'
+export type TelemetryEventLabel = 'init'
+  | 'render_error'
+  | 'ad_load_error'
+  | 'fetch_error'
+  | 'invalid_url_error'
 
 export type HttpRequestMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH'
 
